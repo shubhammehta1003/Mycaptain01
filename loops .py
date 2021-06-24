aa49c89185991c065bef4d912a924bd05062bfd2
shubhammehta1003 <84181294+shubhammehta1003@users.noreply.github.com>
2021-06-25 00:37:13 +0530

    Add files via upload

---
+++ loops .py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+  
+list1 = [12, -7, 5, 64, -14]
+num = 0
+ 
+# using while loop     
+while(num < len(list1)):
+     
+    # checking condition
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+     
+    # increment num 
+    num += 1
