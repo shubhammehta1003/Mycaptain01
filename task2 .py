fa484c66595c05d333b3e7a159b4b9a764ce70d6
shubhammehta1003 <84181294+shubhammehta1003@users.noreply.github.com>
2021-06-25 00:58:00 +0530

    Add files via upload

---
+++ task2 .py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+  
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
+
