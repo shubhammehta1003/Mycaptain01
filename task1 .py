2c8986dcc84342c07692dd1c3ede3c277ff27d82
shubhammehta1003 <84181294+shubhammehta1003@users.noreply.github.com>
2021-06-25 00:54:34 +0530

    Add files via upload

---
+++ task1 .py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+  
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
+
